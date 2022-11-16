959a38140fb826d5b68a98644bddfa7e0e9a4a3e
Jorge Guimaraes <jdguimaraes@hotmail.com>
2022-11-16 17:09:20 +0000

    to finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  new_flat = Flat.new(
+    name: Faker::Name.name_with_middle,
+    address: Faker::Address.full_address,
+    description: Faker::Quote.famous_last_words,
+    price_per_night: rand(20..450),
+    number_of_guests: rand(1..12)
+  )
+  new_flat.save!
+end
